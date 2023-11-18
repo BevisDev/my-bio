d965fe1a901c0e27f8340843085ffa590dafbd92
BevisDev <dev.binhtt@gmail.com>
2023-11-19 00:58:18 +0700

    change package

--- src/screens/MyCV/MainMyCV.js
+++ src/screens/MyCV/MainMyCV.js
@@ -1,10 +1,10 @@
-import HomeCV from "./../../components/mycv/HomeCV";
-import AboutCV from "./../../components/mycv/AboutCV";
+import HomeCV from "../../components/mycv/HomeCV";
+import AboutCV from "../../components/mycv/AboutCV";
 import ExperienceCV from "../../components/mycv/ExperienceCV";
-import EducationCV from "./../../components/mycv/EducationCV";
-import SkillCV from "./../../components/mycv/SkillCV";
-import CertificateListCV from "./../../components/mycv/CertificateListCV";
-import TheEndCV from "./../../components/mycv/TheEndCV";
+import EducationCV from "../../components/mycv/EducationCV";
+import SkillCV from "../../components/mycv/SkillCV";
+import CertificateListCV from "../../components/mycv/CertificateListCV";
+import TheEndCV from "../../components/mycv/TheEndCV";
 
 const MainMyCV = () => {
   return (
