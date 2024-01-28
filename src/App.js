ce3913cf86fb5a2177d54fbdadef5bd0edb88cf1
BevisDev / <dev.binhtt@gmail.com>
2024-01-29 01:51:15 +0700

    create bau_cua_game

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import {
   MY_MOVIE_PATH,
   MY_CV_PATH,
   MY_TODO_PATH,
+  GROUND_CRAB_PATH,
 } from "./constant/PathConst";
 import ToDoPage from "./screens/mytodo/ToDoPage";
+import GroundCrab from "./screens/game/gourdCrab/GourdCrab";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       <Routes>
         <Route path={DASH_BOARD_PATH} element={<DashBoard />}></Route>
         <Route path={MY_CV_PATH} element={<CvPage />}></Route>
+        <Route path={GROUND_CRAB_PATH} element={<GroundCrab />}></Route>
         <Route path={MY_TODO_PATH} element={<ToDoPage />}></Route>
         <Route path={MY_MOVIE_PATH} element={<HomePage />}></Route>
       </Routes>
